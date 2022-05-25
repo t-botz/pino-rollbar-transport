96f3e8b38af18d52f71b636ab2ee0430858b121c
Tibo Delor <delor.thibault@gmail.com>
2022-05-25 20:58:56 +1000

    fix: fix lint and formatting issues

--- test/basic.test.js
+++ test/basic.test.js
@@ -1,23 +1,22 @@
-const test = require("tap").test;
-const pino = require("pino");
+const test = require('tap').test
+const pino = require('pino')
 
-test("v7 transport test", (t) => {
+test('v7 transport test', (t) => {
   const transport = pino.transport({
-    target: "..",
+    target: '..',
     options: {
       rollbarOpts: {
-        enabled: false,
-      },
-    },
-  });
-  const logger = pino(transport);
-  logger.info("hello");
-  transport.flushSync();
-  transport.end();
+        enabled: false
+      }
+    }
+  })
+  const logger = pino(transport)
+  logger.info('hello')
+  transport.flushSync()
+  transport.end()
 
   // If we haven't crashed by then it means :
   // - The transport successfully launched in a worker
   // - logging doesn't make it crash despite no token and being disabled
-  t.end();
-});
-
+  t.end()
+})
